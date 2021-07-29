6d4c09720244f8b4cc94e5cbc7a933e0ee28fbe3
WedgieTheEagle <wsmith02062005@gmail.com>
2021-07-29 21:22:46 +0800

    First Commit

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
-print("hello again how are you")
+print('𝓒𝓪𝓯é 𝓐𝓾 𝓛𝓪𝓲𝓽')
+a = 1
+while a == 1:
+    mode = input('Would you like to order or view the daily report (employee only)')
+    if mode == 'order' or 'Order':
+        
